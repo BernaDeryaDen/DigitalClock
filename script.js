48202f2530bf5572363ab71712381341135c89b8
BernaDeryaDen <brnderyadeniz@gmail.com>
2023-01-10 17:54:06 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let clock = () => {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  var time = hours + ":" + minutes + ":" + seconds + " " + ampm;
+  document.getElementById("clock").innerHTML = time;
+  setTimeout(clock, 1000);
+};
+
+clock();
